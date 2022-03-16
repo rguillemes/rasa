d26701b6d0c1d20642abea4143ab6879056e1015
Raul Guillemes <aa21.rguillemes84@iespabloserrano.com>
2022-03-16 21:32:42 +0100

    primera accion

--- 01-actions/actions/actions.py
+++ 01-actions/actions/actions.py
@@ -4,7 +4,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
+import requests as rq
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
@@ -14,6 +14,18 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+        dispatcher.utter_message(text="La hora actual es:"f"{dt.datetime.now()}")
+
+        return []
+class LlamadaApi(Action):
+    def name(self) -> Text:
+        return "action_call_bus"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        r=rq.get("https://www.zaragoza.es/sede/servicio/monumento.geojson?srsname=wgs84&rows=1&fl=id,title,description,geometry")
+        
+        dispatcher.utter_message(text="El primer bus llega en : "+str(r.json()['features'][0]['properties']['title']))
 
         return []
